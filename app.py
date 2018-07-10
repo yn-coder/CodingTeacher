b5bd8c32eb131c64f222bce85bb1d7abbd026958
yn_coder <yn.coder@gmail.com>
2018-07-10 22:17:34 +0300

    #1 login template

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
 @app.route('/')
 def index():
         all_files = {}
-        return render_template('base.html', notebooks = all_files )
+        return render_template('index.html', notebooks = all_files )
 
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])

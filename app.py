29a1f803752b3804fbc9e05e6240ab97b5b765c0
yn_coder <yn.coder@gmail.com>
2018-07-10 20:15:39 +0300

    #1 test Automatic

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-import os
-import flask
+from flask import Flask, render_template, request, make_response
+from authomatic.adapters import WerkzeugAdapter
+from authomatic import Authomatic
 
-from flask import render_template
+from config_template import CONFIG
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+# Instantiate Authomatic.
+authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
 
 @app.route('/')
 def index():
         all_files = {}
         return render_template('base.html', notebooks = all_files )
+
+
+@app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+def login(provider_name):
+    """
+    Login handler, must accept both GET and POST to be able to use OpenID.
+    """
+
+    # We need response object for the WerkzeugAdapter.
+    response = make_response()
+
+    # Log the user in, pass it the adapter and the provider name.
+    result = authomatic.login(
+        WerkzeugAdapter(
+            request,
+            response),
+        provider_name)
+
+    # If there is no LoginResult object, the login procedure is still pending.
+    if result:
+        if result.user:
+            # We need to update the user to get more info.
+            result.user.update()
+
+        # The rest happens inside the template.
+        return render_template('login.html', result=result)
+
+    # Don't forget to return the response.
+    return response
 
 if __name__ == '__main__':
     #app.run(debug=True)

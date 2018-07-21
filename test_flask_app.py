bd846c23da330894c7e9c4c5b932e4ac4c0efb28
yn_coder <yn.coder@gmail.com>
2018-07-21 17:52:41 +0300

    #14 test flask database

--- test_flask_app.py
+++ test_flask_app.py
@@ -1,24 +1,29 @@
 import os
-import tempfile
+#import tempfile
 
 import pytest
 
-from app import app
-
-
 @pytest.fixture
 def client():
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    #db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    #s = tempfile.mkstemp()
+    os.environ['DATABASE_URL' ] = 'sqlite:////test_db.db'
+    #db_fd, app.config['SQLALCHEMY_DATABASE_URI'] = tempfile.mkstemp()
+    
+    from app import app, db
+    
     app.config['TESTING'] = True
     client = app.test_client()
 
     #with app.app_context():
     #    init_db()
+    db.create_all()
 
     yield client
 
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
+    #os.close(db_fd)
+    #os.unlink(app.config['DATABASE'])
+    #os.unlink(app.config['SQLALCHEMY_DATABASE_URI'])
     
 def test_empty_db(client):
     """Start with a blank database."""

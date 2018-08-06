0b8e3e4a982e8e4daa7f97582950212aa9ecfb9b
yn_coder <yn.coder@gmail.com>
2018-08-06 20:11:54 +0300

    #48 token

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def p():
     if not azure.authorized:
         return 'not'
     else:
-        return '_' + azure.token["expires_at"]
+        return '_' + str(azure.token["expires_at"])
 
 @app.route("/t/")
 def t():
-    return '0000000002'
+    return '0000000003'
 
 if __name__ == '__main__':
 

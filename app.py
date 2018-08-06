819bf1d39c53b8b3f499c3318ad5a6952dfac665
yn_coder <yn.coder@gmail.com>
2018-08-07 00:13:53 +0300

    #48 db

--- app.py
+++ app.py
@@ -2,16 +2,43 @@ from flask import Flask, redirect, url_for
 from werkzeug.contrib.fixers import ProxyFix
 from flask_dance.contrib.azure import make_azure_blueprint, azure
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_dance.consumer.backend.sqla import OAuthConsumerMixin, SQLAlchemyBackend
+
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
-app.secret_key = "supersekrit"
+app.secret_key = "supersekritit_now"
+
 blueprint = make_azure_blueprint(
     client_id="e9bf7a9a-13bc-4db8-af9d-f6626ec9705f",
     client_secret="ekbvXCNU187!golTCD36[#|",
-    scope=["User.Read","profile","openid","email"]
+    scope=["User.Read","profile","openid","email"],
 )
 app.register_blueprint(blueprint, url_prefix="/login")
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+test_sql_url = 'sqlite:////test.db'
+
+if DATABASE_URL:
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = test_sql_url
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=True)
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
+
+if app.config['SQLALCHEMY_DATABASE_URI'] == test_sql_url:
+    db.create_all()
+
+blueprint.backend = SQLAlchemyBackend(OAuth, db.session)
 
 @app.route("/")
 def index():
@@ -32,9 +59,10 @@ def p():
 
 @app.route("/t/")
 def t():
-    return '0000000003'
+    return '0000000006'
 
 if __name__ == '__main__':
 
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
